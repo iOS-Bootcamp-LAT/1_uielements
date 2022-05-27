a3aa57a758cdaba0a12d4cb9ba3e90c0dae80e33
David Granado J <david.granado@interfase.com.bo>
2022-05-27 09:03:27 -0400

    Initial Commit

---
+++ 1_uielements/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1_uielements
+//
+//  Created by David Granado Jordan on 5/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
